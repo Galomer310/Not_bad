c994900dd8e4141e095629e4f8b96e6ddeb473d4
Gal Omer <galomer6708@gmail.com>
2024-09-23 04:19:59 +0300

    bubble sort

---
+++ bubble_sort.js
@@ -0,0 +1,14 @@
+const numbers = [5,0,9,1,7,4,2,6,3,8];
+
+let numString = numbers.toString();
+
+console.log(numString);
+
+let numString2 = numbers.join("+");
+console.log(numString2);
+
+let numString3 = numbers.join(" ");
+console.log(numString3);
+
+let numString4 = numbers.join("");
+console.log(numString4);
